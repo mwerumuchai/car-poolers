725f85fc8fe06ec3fbb163ac37cd72a4220526bd
mwerumuchai <mwerumuchai@gmail.com>
2017-12-01 16:38:12 +0300

    create functions that will render the registration and login template

--- drivers/views.py
+++ drivers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def d_index(request):
+    return render(request, 'd-index.html')
+
+@login_required(login_url='/accounts/register/')
+def d_homepage(request):
+    return render(request, 'd-homepage.html')
+
+def logout(request):
+    return render(request, 'd-index.html')
