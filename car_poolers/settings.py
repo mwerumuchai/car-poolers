495d17b2a729f01d4158fad0094fe7888342061a
mwerumuchai <mwerumuchai@gmail.com>
2017-12-03 22:55:02 +0300

    create database

--- car_poolers/settings.py
+++ car_poolers/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drivers',
-    'riders',
     'bootstrap3',
 ]
 
@@ -79,8 +78,10 @@ WSGI_APPLICATION = 'car_poolers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carpool',
+        'USER': 'muchai',
+        'PASSWORD': 'Ngea!2017##',
     }
 }
 
