0818a93486e3ac0b8c0b5ade75f85efa3408b88b
mwerumuchai <mwerumuchai@gmail.com>
2017-12-03 23:01:46 +0300

    create profile url

--- drivers/urls.py
+++ drivers/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.d_index,name='driverindex'),
-    url(r'^$', views.d_homepage,name='driverhome'),
+    # url(r'^$', views.d_homepage,name='driverhome'),
+    url(r'^about/$', views.about,name='about'),
+    url(r'^profiles/(?P<username>[-_\w.]+)$',views.profile,name='profiles'),
+    url(r'^profiles/edit/(?P<username>[-_\w.]+)$', views.update_profile,name='editprofile'),
 ]
 # if settings.DEBUG:
 #     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
