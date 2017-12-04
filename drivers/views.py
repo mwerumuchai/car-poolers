3b2a8a9fc8a43ffab1a6fd0fa412ac955f1c92c3
mwerumuchai <mwerumuchai@gmail.com>
2017-12-04 09:00:29 +0300

    create view function for car sharing and request ride

--- drivers/views.py
+++ drivers/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.db import transaction
 from django.http import Http404
-from .forms import UserForm,ProfileForm
-from .models import Profile
+from .forms import UserForm,ProfileForm,CarSharingForm
+from .models import Profile,Request
 import datetime as dt
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
@@ -27,6 +27,7 @@ def logout(request):
 def about(request):
     return render(request, 'about.html')
 
+# profile
 @login_required
 @transaction.atomic
 def update_profile(request,username):
@@ -57,3 +58,71 @@ def profile(request,username):
         raise Http404()
 
     return render(request, 'profiles/profile.html', {"user":user, "profile_pic":profile_pic})
+
+# DRIVER
+@login_required
+def carshare(request,user_id):
+    user = User.objects.get(pk=user_id)
+    form = CarSharing(request.POST)
+
+    if user.user.type == 'Rider':
+        raise Http404
+    carsharing = CarDetails.objects.get(pk=cardetails_id, user=user)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            share = form.save(commit=False)
+            share.user = user
+
+            share.start_time = request.POST['start_time']
+            share.cardetails = carsharing
+            share.save()
+
+            rides = CarDetails.objects.filter(user=user).order_by('pk').reverse()
+            shared = CarSharing.objects.filter(user=user).order_by('date').reverse()
+            share = CarSharing.objects.filter(user=user).latest('pk')
+
+            return render(request,'vehicle/view_share.html')
+
+        else:
+            return render(request,'vehicle/share_ride.html', {"user":user, "carsharing":carsharing, "form":form})
+
+    else:
+        return render(request,'vehicle/share_ride.html', {"user":user, "carsharing":carsharing, "form":form})
+
+@login_required
+def see_car_shared(request,user_id):
+    user = User.objects.get(pk=user_id)
+
+    if user.user.type == 'Rider':
+        raise Http404
+    rides = CarDetails.objects.filter(user=user).order_by('pk').reverse()
+    shared = CarSharing.objects.filter(user=user).order_by('date').reverse()
+    request = Request.objects.filter(carsharing=shared)
+
+    return render(request, 'vehicle/view_share.html')
+
+@login_required
+def request_ride(request,user_id,carsharing_id):
+    user = User.objects.get(pk=user_id)
+    carsharing = CarSharing.objects.get(pk=carsharing_id)
+    form = RequestRideForm(reuqest.POST)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            car_request = form.save(commit=False)
+            car_request.user = user
+            car_request.save()
+
+            return redirect('d_index',user_id)
+
+@login_required
+def view_single_ride(request,carsharing_id):
+    carsharing = CarSharing.objects.get(pk=carsharing_id)
+
+    return render(request, 'vehicle/view_one_ride.html', {"carsharing":carsharing})
+
+@login_required
+def view_single_vehicle(reuqest,carsharing_id):
+    carsharing = CarDetails.objects.get(pk=carsharing_id)
+    return render(request, 'vehicle/view_car.html', {"carsharing":carsharing})
