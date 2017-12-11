b8234e2d59d287458b19c9d5c76ccb32c2fe9a1c
mwerumuchai <mwerumuchai@gmail.com>
2017-12-11 09:17:08 +0300

    deploy heroku

--- car_poolers/settings.py
+++ car_poolers/settings.py
@@ -83,6 +83,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'car_poolers.wsgi.application'
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -95,6 +97,10 @@ DATABASES = {
     }
 }
 
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
