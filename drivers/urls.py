f52d116adf755ab14ae499e3326fc59ea4b88ca6
mwerumuchai <mwerumuchai@gmail.com>
2017-12-11 08:52:00 +0300

    create paths for drivers and riders

--- drivers/urls.py
+++ drivers/urls.py
@@ -3,13 +3,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name='drivers'
+app_name = 'drivers'
 
 urlpatterns = [
     url(r'^$', views.d_index,name='driverindex'),
     url(r'^about/$', views.about,name='about'),
-    url(r'^profiles/(?P<username>[-_\w.]+)$',views.profile,name='profiles'),
-    url(r'^profiles/edit/(?P<username>[-_\w.]+)$', views.update_profile,name='editprofile'),
- ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    url(r'^profiles/(?P<username>[-_\w.]+)/$', views.profile,name='driverprofile'),
+    url(r'^profiles/edit/(?P<username>[-_\w.]+)/$', views.update_profile,name='edit'),
+    url(r'^profiles/car/(?P<username>[-_\w.]+)/$', views.car, name='cardetails'),
+    url(r'^profiles/riderprofile/(\d+)/$', views.riderprofile, name='riderprofile'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
