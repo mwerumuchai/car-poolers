ad26c073caf80429ff57f9d198a0f1e954d101a2
mwerumuchai <mwerumuchai@gmail.com>
2017-12-11 08:55:48 +0300

    add map

---
+++ static/js/drivermap.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: -1.28333, lng:  36.81667};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
